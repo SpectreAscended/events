283b2b8a18b7d1bd41d16de08a2f9f6e70782aac
Cory Pollard <astandard87@gmail.com>
2023-02-04 15:31:09 -0600

    Changed events list to handle a case in which there are no events

--- src/components/EventsList.jsx
+++ src/components/EventsList.jsx
@@ -4,24 +4,32 @@ import classes from './EventsList.module.css';
 import formatDate from '../utilities/formatDate';
 
 const EventsList = ({ events }) => {
+  let content = (
+    <h2 className={classes['default-message']}>
+      There are no events. Post something!
+    </h2>
+  );
+
+  if (events) {
+    content = events.map(ev => {
+      return (
+        <li key={ev.id} className={classes.item}>
+          <Link to={`/events/${ev.id}`}>
+            <img src={ev.img} alt={ev.title} />
+            <div className={classes.content}>
+              <h3>{ev.title}</h3>
+              <span>{formatDate(ev.date)}</span>
+            </div>
+          </Link>
+        </li>
+      );
+    });
+  }
+
   return (
     <section className={classes.events}>
       <h2>All Events</h2>
-      <ul className={classes.list}>
-        {events.map(ev => {
-          return (
-            <li key={ev.id} className={classes.item}>
-              <Link to={`/events/${ev.id}`}>
-                <img src={ev.img} alt={ev.title} />
-                <div className={classes.content}>
-                  <h3>{ev.title}</h3>
-                  <span>{formatDate(ev.date)}</span>
-                </div>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+      <ul className={classes.list}>{content}</ul>
     </section>
   );
 };
